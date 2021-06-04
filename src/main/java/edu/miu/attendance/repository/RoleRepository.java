1e4a29ae9965066eaee1809ecad1a13f6de69f35
ENNALI444 <ennali.soufiane01@gmail.com>
2021-06-04 16:27:50 -0500

    Add RoleRepository

--- src/main/java/edu/miu/attendance/repository/RoleRepository.java
+++ src/main/java/edu/miu/attendance/repository/RoleRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.attendance.domain.Role;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, String> {
 
+    Optional<Role> findRoleByByRole(String role);
 }
