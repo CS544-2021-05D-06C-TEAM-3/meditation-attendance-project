d47fcb0750323b7a53fed0b99699e882537d97d6
ENNALI444 <ennali.soufiane01@gmail.com>
2021-06-04 16:27:38 -0500

    Add RegistrationRepository

--- src/main/java/edu/miu/attendance/repository/RegistrationRepository.java
+++ src/main/java/edu/miu/attendance/repository/RegistrationRepository.java
@@ -1,9 +1,13 @@
 package edu.miu.attendance.repository;
 
+import edu.miu.attendance.domain.CourseOffering;
+import edu.miu.attendance.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.attendance.domain.Registration;
 
 public interface RegistrationRepository extends JpaRepository<Registration, String> {
+    Student findByStudent(Student student);
+    CourseOffering findByCourseOffering(CourseOffering courseOffering);
 
 }
