27c448f39f2ce20b2e7c3b60b5c948d8eec5358c
ENNALI444 <ennali.soufiane01@gmail.com>
2021-06-04 16:25:44 -0500

    add CourseSessionRepository

--- src/main/java/edu/miu/attendance/repository/CourseSessionRepository.java
+++ src/main/java/edu/miu/attendance/repository/CourseSessionRepository.java
@@ -1,9 +1,12 @@
 package edu.miu.attendance.repository;
 
+import edu.miu.attendance.domain.CourseOffering;
+import edu.miu.attendance.domain.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.attendance.domain.CourseSession;
 
 public interface CourseSessionRepository extends JpaRepository<CourseSession, String> {
-
+    CourseOffering findByCourseOffering(CourseOffering courseOffering);
+    TimeSlot findByCourseSession(CourseSession courseSession);
 }
