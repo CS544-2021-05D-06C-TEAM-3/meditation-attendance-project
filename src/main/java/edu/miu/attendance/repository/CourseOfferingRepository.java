978c406249e156443aa0298ecdfb55314bad27db
ENNALI444 <ennali.soufiane01@gmail.com>
2021-06-04 16:25:30 -0500

    Add CourseOfferingRepository

--- src/main/java/edu/miu/attendance/repository/CourseOfferingRepository.java
+++ src/main/java/edu/miu/attendance/repository/CourseOfferingRepository.java
@@ -1,9 +1,16 @@
 package edu.miu.attendance.repository;
 
+import edu.miu.attendance.domain.Course;
+import edu.miu.attendance.domain.Faculty;
+import edu.miu.attendance.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.attendance.domain.CourseOffering;
 
+import java.util.List;
+
 public interface CourseOfferingRepository extends JpaRepository<CourseOffering, String> {
 
+    List<CourseOffering> findCourseOfferingByFaculty(Faculty faculty);
+    List<CourseOffering> findCourseOfferingByStudent(Student student);
 }
