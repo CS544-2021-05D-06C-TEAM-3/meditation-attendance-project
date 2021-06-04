361df82b2549d2b7698e1a79140e36771fc81a84
beeAk <se.blen.aklilu@gmail.com>
2021-06-04 15:47:20 -0500

    fix merge conflicts

--- src/main/java/edu/miu/attendance/repository/CourseRepository.java
+++ src/main/java/edu/miu/attendance/repository/CourseRepository.java
@@ -1,8 +1,6 @@
 package edu.miu.attendance.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import edu.miu.attendance.domain.Course;
 
