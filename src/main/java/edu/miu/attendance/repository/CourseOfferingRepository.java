a9554923f523e356d1cf3c1ee2a6f3d5dbe6231c
beeAk <se.blen.aklilu@gmail.com>
2021-06-05 13:41:55 -0500

    Get all registered students by course offering - Get all course offerings for a faculty

--- src/main/java/edu/miu/attendance/repository/CourseOfferingRepository.java
+++ src/main/java/edu/miu/attendance/repository/CourseOfferingRepository.java
@@ -2,6 +2,7 @@ package edu.miu.attendance.repository;
 
 import edu.miu.attendance.domain.Course;
 import edu.miu.attendance.domain.CourseOffering;
+import edu.miu.attendance.domain.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface CourseOfferingRepository extends JpaRepository<CourseOffering, Long> {
 
     List<CourseOffering> getCourseOfferingsByCourse(Course course);
+
+    List<CourseOffering> getCourseOfferingsByFaculty(Faculty faculty);
 }
