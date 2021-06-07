be1434831df2c4d6bfb79ba7a45746b247a7fc39
Mohammed AlDini <51696292+moaldini@users.noreply.github.com>
2021-06-07 14:02:08 -0500

    login

--- src/main/java/edu/miu/attendance/config/SecurityConfig.java
+++ src/main/java/edu/miu/attendance/config/SecurityConfig.java
@@ -1,4 +1,89 @@
 package edu.miu.attendance.config;
 
-public class SecurityConfig {
+import edu.miu.attendance.security.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    @Qualifier("JPAPersonDetailsService")
+    UserDetailsService userDetailsService;
+
+    //@Autowired
+    //JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/", "/index", "/login").permitAll()
+                //.antMatchers("//**").hasAuthority("")
+                //.antMatchers("//**").hasAuthority("")
+                .anyRequest()
+                .authenticated() //all other urls can be access by any authenticated role
+                .and()
+                .formLogin() //enable form login instead of basic login
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**") //don't apply CSRF protection to /h2-console
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/error/access-denied")
+        //.and()
+        //.sessionManagement()
+        //.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
+        //http.addFilterAfter(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        //Those two settings below is to enable access h2 database via browser
+        //http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
 }
