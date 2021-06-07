f97cd24f07ea987c39be75c0f552ba270aadb2a7
beeAk <se.blen.aklilu@gmail.com>
2021-06-07 10:21:16 -0500

    get course offerings of faculty, get barcode records of student registered for course offering

---
+++ src/main/java/edu/miu/attendance/model/FacultyRequest.java
@@ -0,0 +1,12 @@
+package edu.miu.attendance.model;
+
+import lombok.Data;
+
+@Data
+public class FacultyRequest {
+    String firstName;
+    String lastName;
+    String position;
+    String userName;
+    String password;
+}
