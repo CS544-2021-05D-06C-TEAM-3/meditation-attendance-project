0f1425a55297aed936f1e9cc5c773d0a83019d38
beeAk <se.blen.aklilu@gmail.com>
2021-06-08 15:14:31 -0500

    student scan barcode record bug fix

--- src/main/java/edu/miu/attendance/model/BarcodeRequest.java
+++ src/main/java/edu/miu/attendance/model/BarcodeRequest.java
@@ -4,10 +4,12 @@ import edu.miu.attendance.domain.Location;
 import edu.miu.attendance.domain.TimeSlot;
 import lombok.Data;
 
-@Data
+import java.time.LocalDate;
 
+@Data
 public class BarcodeRequest {
 
+    LocalDate date;
     Location location;
     TimeSlot slot;
 }
