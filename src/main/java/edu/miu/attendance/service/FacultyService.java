4df409294316ccd5ef8d049038f2889140d1d2e8
beeAk <se.blen.aklilu@gmail.com>
2021-06-09 22:13:07 -0500

    change faculty from Id to token

--- src/main/java/edu/miu/attendance/service/FacultyService.java
+++ src/main/java/edu/miu/attendance/service/FacultyService.java
@@ -7,13 +7,15 @@ import java.util.List;
 
 public interface FacultyService {
 
+    Faculty findByUsername(String username);
+
     Faculty getFacultyById(long id);
 
     List<Student> getAllStudentForFaculty(long courseOfferingId);
 
-    List<Course> findCoursesByFaculty(long id);
+    List<Course> findCoursesByFaculty();
 
-    List<CourseOffering> findCourseOfferingByFaculty(long id);
+    List<CourseOffering> findCourseOfferingByFaculty();
 
     List<BarcodeRecord> getBarcodeRecordsByCourseOfferingForFaculty(long courseOfferingId, long studentId);
 
